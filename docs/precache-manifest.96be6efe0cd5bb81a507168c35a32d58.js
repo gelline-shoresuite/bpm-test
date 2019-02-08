de4a95282aca61bb6e0f1ef15189f1cfc2a2221f
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-08 13:42:02 +0800

    Updated docs

---
+++ docs/precache-manifest.96be6efe0cd5bb81a507168c35a32d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e4eebd25105ca8287e7",
+    "url": "/bpm-test/static/css/main.dba42156.chunk.css"
+  },
+  {
+    "revision": "2e4eebd25105ca8287e7",
+    "url": "/bpm-test/static/js/main.2e4eebd2.chunk.js"
+  },
+  {
+    "revision": "974b573551a58be5fdcc",
+    "url": "/bpm-test/static/js/1.974b5735.chunk.js"
+  },
+  {
+    "revision": "7b494ea6f7d168a54477",
+    "url": "/bpm-test/static/js/runtime~main.7b494ea6.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/bpm-test/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/bpm-test/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "442968bab58ec45e118fc8edc14bd13e",
+    "url": "/bpm-test/index.html"
+  }
+];
